c258eac114f8b98a09acd30cd38c3319060796cd
Oleh Mushka <olegamysk@gmail.com>
2025-01-20 19:13:48 +0200

    Refactor: remove comments

--- internal/languages/repos/consts.go
+++
@@ -1,8 +0,0 @@
-package repos
-
-const (
-	SchemaName           = "public"
-	FamiliesTableName    = "lang_families"
-	SubfamiliesTableName = "lang_subfamilies"
-	LanguagesTableName   = "lang_languages"
-)
