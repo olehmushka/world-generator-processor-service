479ef27fb091b5a52886cef766e8e0a034c6d993
Oleh Mushka <olegamysk@gmail.com>
2025-01-20 19:12:18 +0200

    Refactor: remove comments

--- core/tools/http_error/structs.go
+++
@@ -1,8 +0,0 @@
-package httperror
-
-type ErrorResp struct {
-	Error        map[string]interface{} `json:"error"`
-	ErrorMessage string                 `json:"error_message"`
-	ErrorCode    string                 `json:"error_code"`
-	TraceID      string                 `json:"trace_id"`
-}
